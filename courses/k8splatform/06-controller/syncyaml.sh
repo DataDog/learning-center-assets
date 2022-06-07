d82e25eb0e6b8befacc85442231035dcb3c0486c
Steve Calnan <64433046+stevedatadog@users.noreply.github.com>
2022-06-06 23:58:07 -0700

    TRAIN-350 last batch of assets (#51)

--- courses/k8splatform/06-controller/syncyaml.sh
+++
@@ -1,6 +0,0 @@
-while [ ! -f "/usr/bin/inotifywait" ]; do
-  sleep 0.2
-done
-while inotifywait -qq -r -e modify,create,delete,move /root/k8s-yaml-files/kube_apiserver_metrics; do
-        rsync -qavz /root/k8s-yaml-files/kube_apiserver_metrics/conf.yaml node01:/root/k8s-yaml-files/kube_apiserver_metrics
-done
