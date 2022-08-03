113db7a9d81a4f7b6f91492e6575557063abf0d4
Alex Rosenkranz <alex.rosenkranz@datadoghq.com>
2022-08-03 16:59:29 -0400

    updates discounts

--- workshops/core-web-vitals-short/discounts.py
+++ workshops/core-web-vitals-short/discounts.py
@@ -27,17 +27,17 @@ def hello():
 @app.route('/discount', methods=['GET', 'POST'])
 def status():
     if flask_request.method == 'GET':
-          discounts = Discount.query.all()
-          app.logger.info(f"Discounts available: {len(discounts)}")
+        discounts = Discount.query.options(joinedload('*')).all()
+        app.logger.info(f"Discounts available: {len(discounts)}")
 
-          influencer_count = 0
-          for discount in discounts:
-              if discount.discount_type.influencer:
-                  influencer_count += 1
-          app.logger.info(f"Total of {influencer_count} influencer specific discounts as of this request")
-          # time experiment. DO NOT RELEASE TO PRODUCTION!!!!
-          time.sleep(2.5)
-          return jsonify([b.serialize() for b in discounts])
+        influencer_count = 0
+        for discount in discounts:
+            if discount.discount_type.influencer:
+                influencer_count += 1
+        app.logger.info(f"Total of {influencer_count} influencer specific discounts as of this request")
+        # time experiment. DO NOT RELEASE TO PRODUCTION!!!!
+        time.sleep(2.5)
+        return jsonify([b.serialize() for b in discounts])
 
     elif flask_request.method == 'POST':
         # create a new discount with random name and value
