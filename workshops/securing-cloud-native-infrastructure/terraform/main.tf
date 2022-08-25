b2f1b6823e7804f828d71bdb71cc9f9951e566b2
Christophe Tafani-Dereeper <christophe.tafanidereeper@datadoghq.com>
2022-08-25 10:09:28 +0200

    missing tf variables

--- workshops/securing-cloud-native-infrastructure/terraform/main.tf
+++ workshops/securing-cloud-native-infrastructure/terraform/main.tf
@@ -9,8 +9,10 @@ module "cloudtrail" {
 module "ecs" {
   source = "../aws-ecs-cluster"
 
-  cluster_name      = "prod-cluster"
-  worker_node_count = 1
-  app_name          = "sample-vulnerable-java-application"
-  ddApiKey          = var.ddApiKey
+  cluster_name             = "prod-cluster"
+  worker_node_count        = 1
+  app_name                 = "sample-vulnerable-java-application"
+  app_ecr_url              = var.app_ecr_url
+  app_task_definition_file = var.app_task_definition_file
+  ddApiKey                 = var.ddApiKey
 }
--- workshops/securing-cloud-native-infrastructure/terraform/variables.tf
+++ workshops/securing-cloud-native-infrastructure/terraform/variables.tf
@@ -1,4 +1,15 @@
+# Notes: These variables are defined here to be propagated to the ECS module
 variable "ddApiKey" {
   type        = string
   description = "Datadog API key to use in the ECS cluster for the agent"
+}
+
+variable "app_ecr_url" {
+  type        = string
+  description = "ECR URL of the application to deploy to the cluster"
+}
+
+variable "app_task_definition_file" {
+  type        = string
+  description = "Path to the application ECS task definition file"
 }
