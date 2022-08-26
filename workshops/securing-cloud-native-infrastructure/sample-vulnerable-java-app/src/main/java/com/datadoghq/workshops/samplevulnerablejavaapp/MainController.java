7efd00eee1585622aa5b481bbf88022ebcd62e97
Christophe Tafani-Dereeper <christophe.tafanidereeper@datadoghq.com>
2022-08-26 16:46:14 +0200

    Add SSRF scenario

--- workshops/securing-cloud-native-infrastructure/sample-vulnerable-java-app/src/main/java/com/datadoghq/workshops/samplevulnerablejavaapp/MainController.java
+++ workshops/securing-cloud-native-infrastructure/sample-vulnerable-java-app/src/main/java/com/datadoghq/workshops/samplevulnerablejavaapp/MainController.java
@@ -17,6 +17,9 @@ public class MainController {
   @Autowired
   private DomainTestService domainTestService;
 
+  @Autowired
+  private WebsiteTestService websiteTestService;
+
   @RequestMapping(method=RequestMethod.POST, value="/test-domain", consumes="application/json")
   public ResponseEntity<String> testDomain(@RequestBody DomainTestRequest request) {
     log.info("Testing domain " + request.domainName);
@@ -30,6 +33,13 @@ public class MainController {
     } catch(Exception e) {
       return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
+  }
+
+  @RequestMapping(method=RequestMethod.POST, value="/test-website", consumes="application/json")
+  public ResponseEntity<String> testWebsite(@RequestBody WebsiteTestRequest request) {
+    log.info("Testing website " + request.url);
+    String result = websiteTestService.testWebsite(request.url);
+    return new ResponseEntity<>(result, HttpStatus.OK);
   }
 
 }
