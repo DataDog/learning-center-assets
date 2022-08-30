b8e88618b6784f74910b2b62a62b5057f4d8814f
Christophe Tafani-Dereeper <christophe.tafanidereeper@datadoghq.com>
2022-08-30 23:24:29 +0200

    Deploy Azure ARM template automatically

--- workshops/accelerate-your-multi-cloud-threat-detection-and-response-with-cloud-siem/terraform/azure.tf
+++ workshops/accelerate-your-multi-cloud-threat-detection-and-response-with-cloud-siem/terraform/azure.tf
@@ -1,10 +1,27 @@
 
-variable "azure_tenant_id" {}
-variable "azure_client_id" {}
-variable "azure_client_secret" {}
-
+# Azure integration for metrics & monitoring
 resource "datadog_integration_azure" "azure-integration" {
   tenant_name   = var.azure_tenant_id
   client_id     = var.azure_client_id
   client_secret = var.azure_client_secret
+}
+
+# Set up logging
+# c.f.
+data "http" "arm-template" {
+  url = "https://raw.githubusercontent.com/DataDog/datadog-serverless-functions/master/azure/deploy-to-azure/parent_template.json"
+}
+resource "azurerm_resource_group_template_deployment" "datadog-logs" {
+  name                = "datadog-azure-logs"
+  resource_group_name = "datadog-azure-logs"
+  deployment_mode     = "Incremental"
+  template_content    = data.http.arm-template.response_body
+  parameters_content = jsonencode({
+    "sendActivityLogs" = {
+      value = true
+    },
+    "apiKey" = {
+      value = var.ddApiKey
+    }
+  })
 }
--- workshops/accelerate-your-multi-cloud-threat-detection-and-response-with-cloud-siem/terraform/versions.tf
+++ workshops/accelerate-your-multi-cloud-threat-detection-and-response-with-cloud-siem/terraform/versions.tf
@@ -4,5 +4,9 @@ terraform {
       source  = "DataDog/datadog"
       version = "3.14.0"
     }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.20.0"
+    }
   }
 }
