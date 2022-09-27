36888cfbd3004531fee6eb14e417df6cef302fc3
Christophe Tafani-Dereeper <christophe.tafanidereeper@datadoghq.com>
2022-09-27 17:03:11 +0200

    Update browse-to-logs.js

--- workshops/securing-cloud-native-infrastructure/browse-to-logs.js
+++ workshops/securing-cloud-native-infrastructure/browse-to-logs.js
@@ -5,11 +5,13 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
   const page = await browser.newPage();
+  console.log("Logging in to Datadog");
   await page.goto('https://app.datadoghq.com');
   await page.type('input[id=username]', process.env.LABUSER);
   await page.type('input[name=password]', process.env.NEWPASSWORD);
   await page.keyboard.press('Enter');
   await page.waitForTimeout(5000);
+  console.log("Browsing to /logs");
   await page.goto('https://app.datadoghq.com/logs')
   await page.waitForTimeout(5000);
   await browser.close();
