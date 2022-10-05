6b7ef53b25dc8acd44b56bd48e19e5099fef15b7
Christophe Tafani-Dereeper <christophe.tafanidereeper@datadoghq.com>
2022-10-05 17:30:35 +0200

    Destroy resource group resource when resource group is destroyed

--- workshops/accelerate-your-multi-cloud-threat-detection-and-response-with-cloud-siem/terraform/providers.tf
+++ workshops/accelerate-your-multi-cloud-threat-detection-and-response-with-cloud-siem/terraform/providers.tf
@@ -1,5 +1,10 @@
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      # Automatically remove resources in a resource group when it's destroyed
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 provider "aws" {
