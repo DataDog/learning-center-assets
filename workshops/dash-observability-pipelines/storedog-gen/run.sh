1db8c4ade940345bc65d00758c2a25786f3aa3f7
Ari Adair <ari.adair@datadoghq.com>
2022-10-10 19:27:08 -0700

    Move the run file into its proper place.

--- workshops/dash-observability-pipelines/storedog-gen/run.sh
+++ workshops/dash-observability-pipelines/storedog-gen/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # This forwards logs from the storedog generator into the Datadog agent
 # directly.
-/root/lab/bin/storedog-gen 2>&1 | nc localhost 10518 &
-disown
+/root/lab/bin/storedog-gen 2>&1 | nc localhost 10518 & disown
