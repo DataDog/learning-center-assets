ad84b7477e018913a0b44d8ba27a3fede0c14c0b
saphoooo <stephane.beuret@gmail.com>
2022-11-18 14:03:28 +0100

    new startup script and datadog.yaml file

--- workshops/services-architect-log-certification/startup.sh
+++ workshops/services-architect-log-certification/startup.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
-set -x
 
-# start 2 services, one producing business logs, the other senstive business logs
-/root/business-app -i 1 >> /root/lab/business-app.log &
-/root/sensitive-app -f sensitive -i 1 >> /root/labsensitive-app.log &
+while true; do
+  case "$1" in
+    --start )
+        if ps aux | grep -q "[b]usiness-app"
+        then
+          echo "business-app is already running"
+        else
+          /root/business-app -i 1 >> /root/lab/business-app.log &
+        fi
+        if ps aux | grep -q "[s]ensitive-app"
+        then
+          echo "sensitive-app is already running"
+        else
+          /root/sensitive-app -f sensitive -i 1 >> /root/labsensitive-app.log &
+        fi
+        break ;;
+    --stop ) pkill business-app 
+        pkill sensitive-app
+        break ;;
+    --restart ) pkill business-app 
+        pkill sensitive-app
+        /root/business-app -i 1 >> /root/lab/business-app.log and &
+        /root/sensitive-app -f sensitive -i 1 >> /root/labsensitive-app.log &
+        break ;;
+    * ) echo "script usage: $(basename $0) [--start|stop|restart]"
+        break ;;
+  esac
+done
