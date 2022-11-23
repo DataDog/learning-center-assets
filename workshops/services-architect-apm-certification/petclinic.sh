7f2e293a94b9fd73f580ebc6efc5a4ed60da0f69
saphoooo <stephane.beuret@gmail.com>
2022-11-23 12:02:42 +0100

    logging for petclinic

--- workshops/services-architect-apm-certification/petclinic.sh
+++ workshops/services-architect-apm-certification/petclinic.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
-
-nohup java -jar target/*.jar &
+  
+while true; do
+  case "$1" in
+    --start )
+        if pgrep java
+        then
+          echo "petclinic is already running"
+        else
+          nohup java -Dserver.port=8090 -jar spring-petclinic/target/*.jar> /root/lab/logs/petclinic.log 2>&1 &
+        fi
+        break ;;
+    --stop ) pkill java
+        break ;;
+    --restart ) pkill business-app
+        pkill java
+        nohup java -Dserver.port=8090 -jar spring-petclinic/target/*.jar> /root/lab/logs/petclinic.log 2>&1 &
+        break ;;
+    * ) echo "script usage: $(basename $0) [--start|stop|restart]"
+        break ;;
+  esac
+done
