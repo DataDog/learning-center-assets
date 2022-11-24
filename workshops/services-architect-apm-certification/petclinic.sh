1380434293008f82d80b14b571aa5296acf0b8c9
saphoooo <stephane.beuret@gmail.com>
2022-11-24 08:25:18 +0100

    update port

--- workshops/services-architect-apm-certification/petclinic.sh
+++ workshops/services-architect-apm-certification/petclinic.sh
@@ -7,13 +7,13 @@ while true; do
         then
           echo "petclinic is already running"
         else
-          nohup java -Dserver.port=8090 -Dlogging.level.org.springframework=DEBUG -jar spring-petclinic/target/*.jar> /root/lab/logs/petclinic.log 2>&1 &
+          nohup java -Dserver.port=8090 -jar spring-petclinic/target/*.jar> /root/lab/logs/petclinic.log 2>&1 &
         fi
         break ;;
     --stop ) pkill java
         break ;;
     --restart ) pkill java
-        nohup java -Dserver.port=8090 -Dlogging.level.org.springframework=DEBUG -jar spring-petclinic/target/*.jar> /root/lab/logs/petclinic.log 2>&1 &
+        nohup java -Dserver.port=8090 -jar spring-petclinic/target/*.jar> /root/lab/logs/petclinic.log 2>&1 &
         break ;;
     * ) echo "script usage: $(basename $0) [--start|stop|restart]"
         break ;;
