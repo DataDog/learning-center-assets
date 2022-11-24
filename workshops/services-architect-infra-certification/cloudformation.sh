9570abdf535fc8e7afa235b432274c6a4f01fa70
saphoooo <stephane.beuret@gmail.com>
2022-11-24 08:47:48 +0100

    first commit

---
+++ workshops/services-architect-infra-certification/cloudformation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Run manually while developing
+# aws cloudformation create-stack \
+#   --stack-name labstack \
+#   --template-body file:///root/lab/storedog-cloudformation.yml \
+#   --capabilities CAPABILITY_NAMED_IAM
