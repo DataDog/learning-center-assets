237cf2d07d8806336d359456d010f0c56a5268e4
Steve Calnan <64433046+stevedatadog@users.noreply.github.com>
2023-01-25 09:58:47 -0800

    TRAIN-837 integrations (#118)

---
+++ courses/intro-integrations/test_awesome_with_integration.py
@@ -0,0 +1,43 @@
+import pytest
+
+    # Don't forget to import your integration
+
+from datadog_checks.awesome import AwesomeCheck
+from datadog_checks.base import ConfigurationError
+
+
+@pytest.mark.unit
+def test_config():
+    instance = {}
+    c = AwesomeCheck('awesome', {}, [instance])
+
+    # empty instance
+    with pytest.raises(ConfigurationError):
+        c.check(instance)
+
+    # only the url
+    with pytest.raises(ConfigurationError):
+        c.check({'url': 'http://foobar'})
+
+    # only the search string
+    with pytest.raises(ConfigurationError):
+        c.check({'search_string': 'foo'})
+
+    # this should not fail
+    c.check({'url': 'http://foobar', 'search_string': 'foo'})
+
+
+@pytest.mark.integration
+@pytest.mark.usefixtures('dd_environment')
+def test_service_check(aggregator, instance):
+    c = AwesomeCheck('awesome', {}, [instance])
+
+    # the check should send OK
+    c.check(instance)
+    aggregator.assert_service_check('awesome.search', AwesomeCheck.OK)
+
+    # the check should send WARNING
+    instance['search_string'] = 'Apache'
+    c.check(instance)
+    aggregator.assert_service_check('awesome.search', AwesomeCheck.WARNING)
+
