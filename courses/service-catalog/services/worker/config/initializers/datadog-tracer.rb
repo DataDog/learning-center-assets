ada6e8ba4d613e04145ec4b8ae3ca9d8f40257f7
Shawn Potts <shawn.potts@datadoghq.com>
2023-02-01 13:41:30 -0800

    updated sub service name

--- courses/service-catalog/services/worker/config/initializers/datadog-tracer.rb
+++ courses/service-catalog/services/worker/config/initializers/datadog-tracer.rb
@@ -3,6 +3,6 @@ Datadog.configure do |c|
   # Activates and configures an integration
   c.tracing.instrument :sidekiq, tag_args: true
   c.tracing.instrument :pg, service_name: 'postgres'
-  c.tracing.instrument :active_support, cache_service: 'store-backend-cache'
+  c.tracing.instrument :active_support, cache_service: 'store-worker-cache'
   c.tracing.instrument :redis, service_name: 'store-worker-redis'
 end
