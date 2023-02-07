83045a03ad3c3f7f89d9add6bb44e4ff9107452e
Shawn Potts <shawn.potts@datadoghq.com>
2023-02-07 15:12:19 -0800

    updated slo script

--- courses/service-catalog/create-slos.sh
+++ courses/service-catalog/create-slos.sh
@@ -1,45 +1,38 @@
 #!/bin/bash
 
 # Declare some variables ahead of time
-declare -A MONITOR_ARRAY
-ALL_MONITORS=""
+ALL_SLOS=""
+SLO=""
 CMD_JQ="/usr/bin/jq"
 CMD_CURL="/usr/bin/curl -sS"
-JSON_DIR="/root/slos/" # location of json files
-# API_URL="https://api.datadoghq.com/api/v1/monitor" # alert
+JSON_DIR="/root/slos" # location of json files
 API_URL="https://api.datadoghq.com/api/v1/slo" # slo
 
-# Read files in JSON_DIR and use jq to get monitor name.
-# Then add filename and monitor name to MONITOR_ARRAY.
-for file in ${JSON_DIR}/*.json
-do
-  MONITOR_ARRAY+=(
-    [${file}]=$( ${CMD_JQ} '.name' ${file} )
-  )
-done
-
-# Get a list of all monitors
-ALL_MONITORS=$(
+# Get a list of all slos
+ALL_SLOS=$(
     ${CMD_CURL} -X GET "${API_URL}" \
     -H "Content-Type: application/json" \
     -H "DD-API-KEY: ${DD_API_KEY}" \
-    -H "DD-APPLICATION-KEY: ${DD_APP_KEY}"
+    -H "DD-APPLICATION-KEY: ${DD_APP_KEY}" \
+    | ${CMD_JQ} '.data[].name'
 )
 
-# Check if the monitor already exists. This avoids duplicates when the lab is ran multiple times.
+# Read files in JSON_DIR and use jq to get slo name.
+# Check if the slo already exists. This avoids duplicates when the lab is ran multiple times.
 # If it doesn't exist, create it.
-for key in ${!MONITOR_ARRAY[@]}
+for file in ${JSON_DIR}/*.json
 do
-  if [[ ${ALL_MONITORS} =~ "${MONITOR_ARRAY[${key}]}" ]]
+  SLO=$( ${CMD_JQ} '.name' ${file} )
+  if [[ ${ALL_SLOS} =~ ${SLO} ]]
   then
-    echo "Monitor exists: ${MONITOR_ARRAY[${key}]}"
+    echo "SLO exists: ${SLO}"
   else
     ${CMD_CURL} -X POST "${API_URL}" \
     -H "Content-Type: application/json" \
     -H "DD-API-KEY: ${DD_API_KEY}" \
     -H "DD-APPLICATION-KEY: ${DD_APP_KEY}" \
-    -d @${key}
+    -d @${file}
     
-    echo "Monitor created: ${MONITOR_ARRAY[${key}]}"
+    echo "SLO created: ${SLO}"
   fi
 done
