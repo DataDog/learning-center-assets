15c321c8a3a4d0a24219eee7a80a1fc4a5dc7533
Shawn Potts <shawn.potts@datadoghq.com>
2023-02-07 15:03:18 -0800

    removed unused variable

--- courses/service-catalog/create-service-definitions.sh
+++ courses/service-catalog/create-service-definitions.sh
@@ -5,7 +5,6 @@ DASHBOARD_URL=""
 CMD_JQ="/usr/bin/jq"
 CMD_CURL="/usr/bin/curl -sS"
 CMD_SED="/usr/bin/sed"
-JSON_DIR="/root/dashboards/" # location of json files
 SERVICE_FILE="/root/lab/service_definitions/services.datadog.yaml"
 DASHBOARD_API_URL="https://api.datadoghq.com/api/v1/dashboard" # dashboard
 SERVICE_API_URL="https://api.datadoghq.com/api/v2/services/definitions" # service definition
