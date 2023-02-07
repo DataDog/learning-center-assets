a154c08ae65a94703576363400e33d1c925a8ac0
Shawn Potts <shawn.potts@datadoghq.com>
2023-02-07 14:53:05 -0800

    updated script

--- courses/service-catalog/create-dashboards.sh
+++ courses/service-catalog/create-dashboards.sh
@@ -1,46 +1,38 @@
 #!/bin/bash
 
 # Declare some variables ahead of time
-declare -A MONITOR_ARRAY
-ALL_MONITORS=""
+ALL_DASHBOARDS=""
+DASHBOARD=""
 CMD_JQ="/usr/bin/jq"
 CMD_CURL="/usr/bin/curl -sS"
-JSON_DIR="/root/dashboards/" # location of json files
+JSON_DIR="/root/dashboards" # location of json files
 API_URL="https://api.datadoghq.com/api/v1/dashboard" # dashboard
-# API_URL="https://api.datadoghq.com/api/v1/monitor" # alert
-# API_URL="https://api.datadoghq.com/api/v1/slo" # slo
 
-# Read files in JSON_DIR and use jq to get monitor name.
-# Then add filename and monitor name to MONITOR_ARRAY.
-for file in ${JSON_DIR}/*.json
-do
-  MONITOR_ARRAY+=(
-    [${file}]=$( ${CMD_JQ} '.title' ${file} )
-  )
-done
-
-# Get a list of all monitors
-ALL_MONITORS=$(
+# Get a list of all dashboards
+ALL_DASHBOARDS=$(
     ${CMD_CURL} -X GET "${API_URL}" \
     -H "Content-Type: application/json" \
     -H "DD-API-KEY: ${DD_API_KEY}" \
-    -H "DD-APPLICATION-KEY: ${DD_APP_KEY}"
+    -H "DD-APPLICATION-KEY: ${DD_APP_KEY}" \
+    | ${CMD_JQ} '.dashboards[].title'
 )
 
-# Check if the monitor already exists. This avoids duplicates when the lab is ran multiple times.
+# Read files in JSON_DIR and use jq to get dashboard name.
+# Check if the dashboard already exists. This avoids duplicates when the lab is ran multiple times.
 # If it doesn't exist, create it.
-for key in ${!MONITOR_ARRAY[@]}
+for file in ${JSON_DIR}/*.json
 do
-  if [[ ${ALL_MONITORS} =~ "${MONITOR_ARRAY[${key}]}" ]]
+  DASHBOARD=$( ${CMD_JQ} '.title' ${file} )
+  if [[ ${ALL_DASHBOARDS} =~ ${DASHBOARD} ]]
   then
-    echo "Monitor exists: ${MONITOR_ARRAY[${key}]}"
+    echo "DASHBOARD exists: ${DASHBOARD}"
   else
     ${CMD_CURL} -X POST "${API_URL}" \
     -H "Content-Type: application/json" \
     -H "DD-API-KEY: ${DD_API_KEY}" \
     -H "DD-APPLICATION-KEY: ${DD_APP_KEY}" \
-    -d @${key}
+    -d @${file}
 
-    echo "Monitor created: ${MONITOR_ARRAY[${key}]}"
+    echo "DASHBOARD created: ${DASHBOARD}"
   fi
 done
