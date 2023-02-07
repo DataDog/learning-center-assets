af022fd0557854f24590d43aba3ec543d15ce8fe
Shawn Potts <shawn.potts@datadoghq.com>
2023-02-07 15:01:09 -0800

    updated incidents script

--- courses/service-catalog/create-incidents.sh
+++ courses/service-catalog/create-incidents.sh
@@ -1,44 +1,38 @@
 #!/bin/bash
 
 # Declare some variables ahead of time
-declare -A MONITOR_ARRAY
-ALL_MONITORS=""
+ALL_INCIDENTS=""
+INCIDENT=""
 CMD_JQ="/usr/bin/jq"
 CMD_CURL="/usr/bin/curl -sS"
-JSON_DIR="/root/incidents/" # location of json files
+JSON_DIR="/root/incidents" # location of json files
 API_URL="https://api.datadoghq.com/api/v2/incidents" # incident
 
-# Read files in JSON_DIR and use jq to get monitor name.
-# Then add filename and monitor name to MONITOR_ARRAY.
-for file in ${JSON_DIR}/*.json
-do
-  MONITOR_ARRAY+=(
-    [${file}]=$( ${CMD_JQ} '.data.attributes.title' ${file} )
-  )
-done
-
-# Get a list of all monitors
-ALL_MONITORS=$(
+# Get a list of all incidents
+ALL_INCIDENTS=$(
     ${CMD_CURL} -X GET "${API_URL}" \
     -H "Content-Type: application/json" \
     -H "DD-API-KEY: ${DD_API_KEY}" \
-    -H "DD-APPLICATION-KEY: ${DD_APP_KEY}"
+    -H "DD-APPLICATION-KEY: ${DD_APP_KEY}" \
+    | ${CMD_JQ} '.data[].attributes.title'
 )
 
-# Check if the monitor already exists. This avoids duplicates when the lab is ran multiple times.
+# Read files in JSON_DIR and use jq to get incident name.
+# Check if the incident already exists. This avoids duplicates when the lab is ran multiple times.
 # If it doesn't exist, create it.
-for key in ${!MONITOR_ARRAY[@]}
+for file in ${JSON_DIR}/*.json
 do
-  if [[ ${ALL_MONITORS} =~ "${MONITOR_ARRAY[${key}]}" ]]
+  INCIDENT=$( ${CMD_JQ} '.data.attributes.title' ${file} )
+  if [[ ${ALL_INCIDENTS} =~ ${INCIDENT} ]]
   then
-    echo "Monitor exists: ${MONITOR_ARRAY[${key}]}"
+    echo "Incident exists: ${INCIDENT}"
   else
     ${CMD_CURL} -X POST "${API_URL}" \
     -H "Content-Type: application/json" \
     -H "DD-API-KEY: ${DD_API_KEY}" \
     -H "DD-APPLICATION-KEY: ${DD_APP_KEY}" \
-    -d @${key}
+    -d @${file}
 
-    echo "Monitor created: ${MONITOR_ARRAY[${key}]}"
+    echo "Incident created: ${INCIDENT}"
   fi
 done
