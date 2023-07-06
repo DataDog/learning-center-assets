fb86252a972ba03d28d371fd32a9605971aa6213
saphoooo <stephane.beuret@gmail.com>
2023-07-06 11:58:50 +0200

    first commit

---
+++ workshops/dd101-for-engineering-team/word.sql
@@ -0,0 +1,41 @@
+CREATE TABLE WORDS(
+   ID INT PRIMARY KEY      NOT NULL,
+   KEYWORD           CHAR(10) NOT NULL,
+   QUOTE         TEXT      NOT NULL
+);
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (1, 'rose', 'A rose by any other name would smell as sweet. -- William Shakespeare');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (2, 'gold', 'All that glitters is not gold. -- William Shakespeare');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (3, 'gold', 'As good as gold. -- Charles Dickens');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (4, 'gold', 'Governments lie; bankers lie; even auditors sometimes lie: gold tells the truth. -- William Rees Mogg');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (5, 'gold', 'Never trust money more than gold. -- Toba Beta');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (6, 'money', 'Rule No.1 is never lose money. Rule No.2 is never forget rule number one. -- Warren Buffett');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (7, 'money','If you want to be rich, simply serve more people -- Robert Kiyosaki');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (8, 'money','Too many people spend money they earnet...to buy things they don''t want...to impress people that they don''t like. -- Will Rogers');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (9, 'money','Money is a terrible master but an excellent servant. --P.T. Barnum');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (10, 'money','Fortune sides with him who dares. -- Virgil');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (11, 'money','If we command our wealth, we shall be rich and free. If our wealth commands us, we are poor indeed. -- Edmund Burke');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (12, 'money','We make a living by what we get, but we make a life by what we give. -- Winston Churchill');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (13, 'money','Never spend your money before you have it.  -- Thomas Jefferson');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (14, 'money','You must gain control over your money or the lack of it will forever control you. -- Dave Ramsey');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (15, 'money','How many millionaires do you know who have become wealthy by investing in savings accounts? I rest my case. -- Robert G. Allen');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (16, 'money','A wise person should have money in their head, but not in their heart.” -- Jonathan Swift');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (17, 'money','Money is a reward for solving problems. -- Mike Murdock');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (18, 'money','Success is not how many zeroes your bank account has. It''s about making the most of the life you have. -- SuzeOrman');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (19, 'money','Money is usually attracted, not pursued. -- Jim Rohn');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (20, 'money','The reason I''ve been able to be so financially successful is my focus has never, ever for one minute been money. -- Oprah Winfrey');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (21, 'money','It is not how much you earn but what you do with it that determines your end. -- Olumide Emmanuel');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (22, 'trouble','If there must be trouble, let it be in my day, that my child may have peace. -- Thomas Paine');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (23, 'trouble','That''s the real trouble with the world, too many people grow up. -- Walt Disney');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (24, 'trouble','The trouble with most of us is that we would rather be ruined by praise than saved by criticism. -- Norman Vincent Peale');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (25, 'trouble','roubles are often the tools by which God fashions us for better things. -- Henry Ward Beecher');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (26, 'trouble','Worry is interest paid on trouble before it comes due. -- William Ralph Inge');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (27, 'trouble','Better to lose count while naming your blessings than to lose your blessings to counting your troubles. -- Maltbie Davenport Babcock');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (28, 'trouble','If you could kick the person in the pants responsible for most of your trouble, you wouldn''t sit for a month. -- Theodore Roosevelt');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (29, 'trouble','Do not trouble trouble till trouble troubles you. -- Robert Jordan');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (30, 'humor','Two things are infinite: the universe and human stupidity; and I''m not sure about the universe. -- Albert Einstein');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (31, 'humor','So many books, so little time. -- Frank Zappa');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (32, 'humor','You only live once, but if you do it right, once is enough. -- Mae West');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (33, 'humor','Insanity is doing the same thing, over and over again, but expecting different results. -- Narcotics Anonymous');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (34, 'humor','The person, be it gentleman or lady, who has not pleasure in a good novel, must be intolerably stupid. -- Jane Austen, Northanger Abbey');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (35, 'humor','Outside of a dog, a book is man''s best friend. Inside of a dog it''s too dark to read. -- Groucho Marx');
+INSERT INTO WORDS (ID, KEYWORD, QUOTE) VALUES (36, 'humor','A day without sunshine is like, you know, night. -- Steve Martin');
